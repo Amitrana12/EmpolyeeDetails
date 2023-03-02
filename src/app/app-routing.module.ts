6eb97d13898b1005d013e10209e9d7f2b01da6db
Amit Rana <Amirana14325@gmail.com>
2023-03-02 10:16:17 +0530

    CURD opration with API call

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StudentComponent } from "./student/student.component";
+import { DepartmentComponent } from "./department/department.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'student', component:StudentComponent},
+  {path:'department', component:DepartmentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
